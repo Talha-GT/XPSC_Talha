578bf1eef4031dc220107c99023b43f635fdf4ef
Talha-GT <150540113+Talha-GT@users.noreply.github.com>
2024-03-16 23:55:03 +0600

    Update r.cpp

--- Week 1/r.cpp
+++ Week 1/r.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main() {
     string s, ss;
-    ss = "abcdefghijklmnopqrstuvwxyz"; // Initialize ss as a string
+    ss = "abcdefghijklmnopqrstuvwxyz";
     cin >> s;
     set<char> st; // Declare set of characters
-    for (int i = 0; i < s.size(); i++) {
-        st.insert(s[i]); // Use insert instead of push for sets
+    for (int i = 0; i < s.size(); i++)
+    {
+        st.insert(s[i]); 
     }
     for (auto val : st) {
         cout << val << " ";
     }
-    cout << endl << ss << endl; // Print ss on a new line
+    cout << endl << ss << endl;
     return 0;
 }
 
